f6f7116a86d2d92c4227383d64aaa1b73a8dc279
zfletch <zfletch2@gmail.com>
2018-07-05 15:09:55 -0400

    port config

--- lib/config.rb
+++ lib/config.rb
@@ -1,4 +1,5 @@
 module Config
   MORPHLIB = ENV.fetch('MORPHLIB') { 'stemlib' }
   EXECUTABLE = ENV.fetch('EXECUTABLE') { 'bin/cruncher' }
+  PORT = ENV.fetch('PORT') { '1315' }
 end
--- server.rb
+++ server.rb
@@ -4,6 +4,7 @@ require './lib/morpheus'
 
 set :protection, except: [:path_traversal]
 set :morpheus, Morpheus.new(Config::MORPHLIB, Config::EXECUTABLE)
+set :port, Config::PORT
 
 get "/greek/:word" do
   settings.morpheus.response(params[:word], params[:opts], :Greek)
